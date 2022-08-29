812b03284c4170e887eb18459ac77b120ee8eab3
Krzysztof <klobaziewicz@wp.pl>
2022-08-29 23:02:16 +0200

    next commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import createStore from 'redux'
+
+const data = {
+  produkty: [
+    'produkt 1', 'produkt 2', 'produkt 3'
+  ]
+}
+
+//reducer
+function counter(state = data, action) {
+  switch (action.type) {
+    default: return state
+  }
+}
+
+const store = createStore(produkty)
+window.store = store
 
 function App() {
   return (
