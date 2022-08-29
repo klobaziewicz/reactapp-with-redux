a52e45c30a192ed759f9e4dc2c4d0f620a1014c5
Krzysztof <klobaziewicz@wp.pl>
2022-08-29 23:11:43 +0200

    next commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>redux</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
